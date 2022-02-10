9f5d0a5fc288f3019982583334f42ba050a44c16
patelanup84 <patelanup84@gmail.com>
2022-02-10 09:52:23 -0700

    main

--- distillery.py
+++ distillery.py
@@ -13,21 +13,12 @@ st.write("This application will add email activity (sends, opens, clicks) to the
 # ---------------------------------#
 # User Functions
 
-# function to select files  
-def file_selector(folder_path='inputs/ai master file/'):
-    filenames = os.listdir(folder_path)
-    selected_filename = st.selectbox('Select AI Master Grower Data File to Load', filenames)
-    return os.path.join(folder_path, selected_filename)
-
 
 
 # ---------------------------------#
 # Main panel
 
-
-filename = file_selector()
-st.write('You selected `%s`' % filename)
-
-# reading master file csv display first 5 rows
-df_grower = pd.read_csv('inputs/ai master file/2022 AI Grower Master Data File Jan 28 - modifed.csv')
-st.write(df_grower.head())
+uploaded_file = st.file_uploader("Load latest AI Master File")
+if uploaded_file is not None:
+  df = pd.read_csv(uploaded_file)
+  st.write(dataframe)
