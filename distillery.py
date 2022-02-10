60e923c6c25b70f0c22d82f872fdd721d154f909
patelanup84 <patelanup84@gmail.com>
2022-02-10 08:41:06 -0700

    main

--- distillery.py
+++ distillery.py
@@ -1,5 +1,11 @@
 import streamlit as st
+
+import pandas as pd
 import os
+import numpy as np
+import re
+
+
 
 def file_selector(folder_path='inputs/ai master file/'):
     filenames = os.listdir(folder_path)
@@ -8,3 +14,6 @@ def file_selector(folder_path='inputs/ai master file/'):
 
 filename = file_selector()
 st.write('You selected `%s`' % filename)
+
+df_grower = pd.read_csv(filename)
+df_grower.head(3)
