11eff8a25c837da850501c3ba1cf61f4cfb52f7b
patelanup84 <patelanup84@gmail.com>
2022-02-10 08:36:47 -0700

    main

--- distillery.py
+++ distillery.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import os
 
-def file_selector(folder_path='inputs/'):
+def file_selector(folder_path='inputs/ai master file/'):
     filenames = os.listdir(folder_path)
     selected_filename = st.selectbox('Select AI Master Grower Data File to Load', filenames)
     return os.path.join(folder_path, selected_filename)
