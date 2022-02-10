45b8507014b3cffbfc859ecad376d9c226125569
patelanup84 <patelanup84@gmail.com>
2022-02-10 10:07:06 -0700

    main

--- distillery.py
+++ distillery.py
@@ -14,7 +14,26 @@ st.write("This application will add email activity (sends, opens, clicks) to the
 
 # ---------------------------------#
 # User Functions
+# function to format/standardize dataframes for processing:
+def format_dataframe(df):
+  
+  # Format col. headers (snake case)
+  # df = df.rename(columns={element: re.sub(r'([A-Z]+)', r'_\1', element) for element in df.columns.tolist()})# add space before cap. letter
+  df = df.rename(columns={element: re.sub(r'(?<![A-Z])(?<!^)([A-Z])',r' \1', element) for element in df.columns.tolist()})
+  df.columns = df.columns.str.lstrip('_') #strip leading underscore
+  df.columns = df.columns.str.lower() # convert to lowercase
+  df.columns = map(lambda x : x.replace("-", "_").replace(" ", "_"), df.columns) # replace hyphens/spaces with underscore
+  df.columns = map(lambda x : x.replace("__", "_"), df.columns) # replace double underscore with single underscore
+  df.columns = df.columns.str.strip() # strip leading and trailing whitespaces
+  
+  specchar = '[≅,#,@,&,(,),?,\']' # List of spec. chars
+  df.columns = df.columns.str.replace(specchar, '') # Remove spec. char.
 
+  # Format row data
+  df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x) # strip leading and trailing whitespaces
+  df = df.applymap(lambda x: x.upper() if isinstance(x, str) else x) # convert string values to uppercase
+
+  return df
 
 
 # ---------------------------------#
@@ -22,6 +41,8 @@ st.write("This application will add email activity (sends, opens, clicks) to the
 
 uploaded_file = st.file_uploader("Load latest AI Master File")
 if uploaded_file is not None:
-  df = pd.read_csv(uploaded_file)
-  df = df.astype(str)
-  st.write(df)
+  df_grower = pd.read_csv(uploaded_file)
+  df_grower = df.astype(str)
+  df_grower = format_dataframe(df_grower)
+  st.write(df_grower)
+
