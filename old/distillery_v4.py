130da52173125ae3d10653f222f6b1d73a28609c
patelanup84 <patelanup84@gmail.com>
2022-02-17 10:07:18 -0700

    main

--- old/distillery_v4.py
+++ old/distillery_v4.py
@@ -13,6 +13,8 @@ from meteostat import Point, Monthly
 
 # ---------------------------------#
 ### THEME SETTINGS ###
+primary_clr = st.get_option("theme.primaryColor")
+txt_clr = st.get_option("theme.textColor")
 
 
 # ---------------------------------#
@@ -82,49 +84,47 @@ st.write("This prototype is designed to test user functionaility and better unde
 
 #Step 1. Load Grower Master file
 st.header('Step 1. Load Grower Master File')
-# st.write("Select the number of growers to match. The higher the number, the longer it will take to process (and could potentially timeout). Recommended value is 50-100, which will take apprx. 5 mins to run through each of the steps. Default = 50") 
-# x = st.slider('Select a value: ',10,1000,50)
+st.write("Select the number of growers to match. The higher the number, the longer it will take to process (and could potentially timeout). Recommended value is 50-100, which will take apprx. 5 mins to run through each of the steps. Default = 50") 
+x = st.slider('Select a value: ',10,1000,50)
 
 
 uploaded_file = st.file_uploader('Load the latest grower master file.csv')
-
 if uploaded_file is not None:
-  # load selected file
-  df_grower = pd.read_csv(uploaded_file)
+    
+    # load selected file
+    df_grower = pd.read_csv(uploaded_file)
 
-  st.write("Select the number of growers to match. The higher the number, the longer it will take to process (and could potentially timeout). Recommended value is 50-100, which will take apprx. 5 mins to run through each of the steps. Default = 50") 
-  x = st.slider('Select a value: ',10,1499,100)
-  
-  # select small sample size
-  df_grower = df_grower.sample(n=x)
-  # Format/standardize dataframe
-  df_grower = format_dataframe(df_grower)
+    # select small sample size
+    df_grower = df_grower.sample(n=x)
 
-  ## FORMAT/CLEAN DATA
-  # replace 0 with blanks for select cols.
-  list_cols = ['grower_site_id','grower_site_name','grower_first_name','grower_last_name','grower_email','grower_phone','grower_cellular','grower_contact_phone','grower_contact_cell']
-  df_grower[list_cols] = df_grower[list_cols].replace({'0':np.nan, 0:np.nan,'':np.nan})
+    # Format/standardize dataframe
+    df_grower = format_dataframe(df_grower)
 
-  # remove all numeric values from select cols.
-  list_cols = ['2021_sales','grower_phone','grower_cellular','grower_contact_phone','grower_contact_cell']
-  df_grower[list_cols] = df_grower[list_cols].replace('[^\d.]', '', regex = True).replace('',np.nan)
+    ## FORMAT/CLEAN DATA
+    # replace 0 with blanks for select cols.
+    list_cols = ['grower_site_id','grower_site_name','grower_first_name','grower_last_name','grower_email','grower_phone','grower_cellular','grower_contact_phone','grower_contact_cell']
+    df_grower[list_cols] = df_grower[list_cols].replace({'0':np.nan, 0:np.nan,'':np.nan})
 
-  # convert select cols to numeric(float)
-  list_cols = ['2021_sales']
-  df_grower[list_cols] = df_grower[list_cols].astype(float)
+    # remove all numeric values from select cols.
+    list_cols = ['2021_sales','grower_phone','grower_cellular','grower_contact_phone','grower_contact_cell']
+    df_grower[list_cols] = df_grower[list_cols].replace('[^\d.]', '', regex = True).replace('',np.nan)
 
-  # display No. of growers
-  num_growers = df_grower['grower_site_id'].nunique()
-  st.write("Total No. of Growers in Database=",num_growers)
-  
-  # display % with emails
-  num_emails = df_grower['grower_email'].nunique()
-  per_emails = round(((num_emails/num_growers)*100),0)
-  st.write("Percent of growers with emails=",per_emails)
+    # convert select cols to numeric(float)
+    list_cols = ['2021_sales']
+    df_grower[list_cols] = df_grower[list_cols].astype(float)
 
-  # display dataframe
-  st.write("**Original Grower Data File**")
-  st.write(df_grower)
+    # display No. of growers
+    num_growers = df_grower['grower_site_id'].nunique()
+    st.write("Total No. of Growers in Database=",num_growers)
+    
+    # display % with emails
+    num_emails = df_grower['grower_email'].nunique()
+    per_emails = round(((num_emails/num_growers)*100),0)
+    st.write("Percent of growers with emails=",per_emails)
+
+    # display dataframe
+    st.write(df_grower)
+
 
 
 #Step 2. Import & Match Email Data
@@ -132,7 +132,6 @@ st.header('Step 2. Import & Match Email Data')
 st.write('Press the button below to upload the historical email activity from SFMC & Act-on')
 
 submit = st.button('Match Email Data')
-
 if submit:
   # Load formatted & unstacked email data
   filepath = 'inputs/email/emails_formatted.csv'
@@ -143,46 +142,50 @@ if submit:
       df_emails.loc[df_emails['action_date'].between(startdate,enddate), 'program_year'] = k
   # Drop cols
   df_emails = df_emails.drop(['action_date'],axis=1,inplace=False)
-  # Pivot out by 'program yr' then 'action'
-  df_grower_email = pd.pivot_table(df_emails, 
-                                   index=['grower_email'], 
-                                   columns=['program_year','action'], 
-                                   aggfunc=len)
-  # Flatten pivot table to dataframe
-  df_grower_email.columns = df_grower_email.columns.to_series().str.join('_')
-  # Convert headers to lower case
-  df_grower_email.columns = df_grower_email.columns.str.lower()
-
-  ## ADD NEW FEATURES
-  # add 'total_sent' col.
-  list_sent = [col for col in df_grower_email.columns if 'sent' in col]
-  df_grower_email['total_sent'] = df_grower_email[list_sent].sum(axis=1)
-  # add 'total_opened' col.
-  list_opened = [col for col in df_grower_email.columns if 'opened' in col]
-  df_grower_email['total_opened'] = df_grower_email[list_opened].sum(axis=1)
-  # add 'total_clicked' col.
-  list_clicked = [col for col in df_grower_email.columns if 'clicked' in col]
-  df_grower_email['total_clicked'] = df_grower_email[list_clicked].sum(axis=1)
-  # add 'avg_open_rate' col.
-  df_grower_email['avg_open_rate'] = round((df_grower_email['total_opened']/df_grower_email['total_sent'])*100,1)
-  # add 'avg_ctr' col.
-  df_grower_email['avg_ctr'] = round((df_grower_email['total_clicked']/df_grower_email['total_sent'])*100,1)
-  # add 'avg_ctor' col.
-  df_grower_email['avg_ctor'] = round((df_grower_email['total_clicked']/df_grower_email['total_opened'])*100,1)
- 
+  # Pivot by Grower
+  df_grower_email = pd.pivot_table(df_emails, index=['grower_email','program_year'], columns = 'action',aggfunc=len).reset_index()
+  # Format cols.
+  df_grower_email.columns = df_grower_email.columns.str.lower() # convert to lowercase
+  # Reorder columns
+  list_col = ['grower_email','program_year','sent','opened','clicked']
+  df_grower_email = df_grower_email.reindex(columns=list_col)
+  # Rename columns
+  df_grower_email = df_grower_email.rename({'sent':'emails_sent','opened':'emails_opened','clicked':'emails_clicked',},axis=1)
+  # Reset index
+  df_grower_email = df_grower_email.reset_index(drop=True)
+  
+  # Group by Prog. years
+  df_grower_email_by_year = df_grower_email.groupby(['program_year'])['emails_sent','emails_opened','emails_clicked'].sum().reset_index()
+  # add open rates and ctr                               
+  df_grower_email_by_year['open_rate'] = round((df_grower_email_by_year['emails_opened']/df_grower_email_by_year['emails_sent'])*100,2)
+  df_grower_email_by_year['ctr'] = round((df_grower_email_by_year['emails_clicked']/df_grower_email_by_year['emails_sent'])*100,2)
+  
   #MERGE WITH 2022 GROWER DATA
-  df_cdp_email = df_grower.merge(df_grower_email, on='grower_email', how='left')
+  # filter for program year = 2021
+  df_grower_email_2021 = df_grower_email[df_grower_email['program_year']=='2021']
+
+  #rename columns
+  df_grower_email_2021 = df_grower_email_2021.rename({'emails_sent':'emails_sent_2021',
+                                                      'emails_opened':"emails_opened_2021",
+                                                      'emails_clicked':'emails_clicked_2021'},axis=1)
+  # drop cols
+  df_grower_email_2021 = df_grower_email_2021.drop(['program_year'],axis=1,inplace=False)
+  
+  #create CDP and merge email data
+  df_cdp = df_grower.merge(df_grower_email_2021, on='grower_email', how='left')
+  st.write("Email data matching completed.")
 
   st.write("**Email Data Analysis**")
-  num_email_activty = df_cdp_email['total_sent'].count()
-  st.write("Total # of growers matched with emails =",num_email_activty)
-  per_emails_matched_total = round(((num_email_activty/num_growers)*100),2)
-  st.write("% of total growers in list matched =",per_emails_matched_total)
-  per_emails_matched = round(((num_email_activty/num_emails)*100),2)
-  st.write("% of growers with emails matched =",per_emails_matched)
+  st.write("Total # of growers matched=","TBD")
+  st.write("% of growers with emails matched=","TBD")
+  st.write("% of total growers in list matched=","TBD")
 
-  st.write("**Grower Data, Enhanced with Email Activity**")
-  st.write(df_cdp_email)
+  # Display dataframe
+  st.write("**Breakdown of Email Performance, Program YoY**")
+  st.write(df_grower_email_by_year)
+
+  st.write("**Grower File, Enhanced with Email Data**")
+  st.write(df_cdp)
 
 
 #Step 3. Import & Match Weather Data
@@ -195,6 +198,7 @@ if submit:
   # Create list of coordinates
   list_coord = df_grower['site_coord'].tolist()
   num_coord = len(list_coord)
+  st.write("No. of locations to gather weather data for=",num_coord)
 
   # Create a placeholder list to hold results
   results = []
@@ -217,34 +221,22 @@ if submit:
                                   'site_avg_temp':"site_avg_temp_2021",},axis=1)
 
   #MERGE WITH 2022 GROWER DATA
-  df_cdp_weather = df_grower.merge(df_weather, on='site_coord', how='left')
-  df_cdp_weather = df_cdp_weather.drop_duplicates()
+
+  df_cdp = df_grower.merge(df_weather, on='site_coord', how='left')
 
   st.write("Weather data matching completed.")
 
   st.write("**Weather Data Analysis**")
-  num_prec_matched = df_cdp_weather['2021_site_avg_prec'].count()
-  st.write("Total # of growers matched =",num_prec_matched)
-  per_emails_matched_total = round(((num_prec_matched/num_growers)*100),2)
-  st.write("% of growers from list matched =",per_emails_matched_total)
+  st.write("Total # of growers matched=","TBD")
+  st.write("% of growers matched=","TBD")
 
   # Display dataframe
   st.write("**Grower File, Enhanced with Weather Data**")
-  st.write(df_cdp_weather)
+  st.write(df_cdp)
 
 #Step 4. Analyze Results
 st.header('Step 4. Analyze Results')
 st.write('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua')
-
-submit = st.button('Analyze')
-if submit:
-  #MERGE WITH 2022 GROWER DATA
-  df_cdp = df_cdp_email.merge(df_weather, on='site_coord', how='left')
-  
-  # Display dataframe
-  st.write("**Grower File, Enhanced with Email & Weather Data**")
-  st.write(df_cdp)
-
 
 #Step 5. Export Data
 st.header('Step 5. Export Data')
